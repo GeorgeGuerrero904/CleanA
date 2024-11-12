4fd32ab75a8e460ebaa860bd63e7e84875e03895
Jorge <jorge.guerrero@sitel.com>
2024-11-12 14:32:01 -0500

    adding MediatR and AutoMapper

---
+++ GloboTicket.TicketManagement.Application/Features/Events/GetEventsListQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events
+{
+    public class GetEventsListQueryHandler : IRequestHandler<GetEventsListsQuery, List<EventListVm>>
+    {
+        public Task<List<EventListVm>> Handle(GetEventsListsQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GloboTicket.TicketManagement.Application/Features/Profiles/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Events;
+using GloboTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Features.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() { 
+            CreateMap<Event, EventListVm>().ReverseMap();
+            CreateMap<Event, EventDetailVm>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+        }
+    }
+}
