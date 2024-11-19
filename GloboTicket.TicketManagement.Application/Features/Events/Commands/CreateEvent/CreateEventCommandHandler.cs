456658f1d0c0b5d1dd64018320edc34e6eadd305
Jorge <jorge.guerrero@sitel.com>
2024-11-19 16:46:08 -0500

    adding ifrastructure project and persitence

--- GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
 using GloboTicket.TicketManagement.Application.Contracts.Persistance;
+using GloboTicket.TicketManagement.Application.Models.Mail;
 using GloboTicket.TicketManagement.Domain.Entities;
 using MediatR;
 
@@ -9,11 +11,13 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
     {
         private readonly IEventRepository _eventRepository;
         private readonly IMapper _mapper;
+        private readonly IEmailService _emailService;
 
-        public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository)
+        public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository, IEmailService emailService)
         {
             _mapper = mapper;
             _eventRepository = eventRepository;
+            _emailService = emailService;
         }
 
         public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
@@ -25,9 +29,16 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
 
             if (validationResult.Errors.Count > 0)
                 throw new Exceptions.ValidationException(validationResult);
-
             @event = await _eventRepository.AddAsync(@event);
 
+            //sending an email to admin address
+            var email = new Email() { To = "Gill@snowball.be", Body = $"A new event was created: {request}", Subject = "A new event was created" };
+            try
+            {
+                await _emailService.SendEmail(email);
+            }
+            catch (Exception ex)
+            {}
             return @event.EventId;
         }
     }
